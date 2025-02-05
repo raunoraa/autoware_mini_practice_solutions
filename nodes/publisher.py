f3a124b50f5d017ccb95568d5bc49bbb620b34ac
rauno <raunoraa13@gmail.com>
2025-02-05 15:40:34 +0200

    Practice 1 complete!

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -3,10 +3,14 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('publisher')
-rate = rospy.Rate(2)
-pub = rospy.Publisher('/message', String, queue_size=10)
+rospy.init_node("publisher")
+
+freq = rospy.get_param("~freq", 2)
+rate = rospy.Rate(freq)
+
+pub = rospy.Publisher("/message", String, queue_size=10)
+message = rospy.get_param("~message", "Hello World!")
 
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
--- nodes/subscriber.py
+++ nodes/subscriber.py
@@ -3,9 +3,11 @@
 import rospy
 from std_msgs.msg import String
 
+
 def callback(msg):
     print(msg.data)
 
-rospy.init_node('subscriber')
+
+rospy.init_node("subscriber")
 rospy.Subscriber("/message", String, callback)
 rospy.spin()
