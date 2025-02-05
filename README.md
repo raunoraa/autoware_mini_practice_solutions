91adb78bfd97ae6abfcefd4e9c131304dffb7f43
rauno <raunoraa13@gmail.com>
2025-02-05 14:09:31 +0200

    Added multiple things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Autoware Mini Practice Solutions
+
 Autoware Mini course's tasks solutions are stored here.
---
+++ nodes/subscriber.py
@@ -0,0 +1,9 @@
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+    print(msg.data)
+
+rospy.init_node('subscriber')
+rospy.Subscriber("/message", String, callback)
+rospy.spin()
