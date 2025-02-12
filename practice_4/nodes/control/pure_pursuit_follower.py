c24ea05295f26b8aba5b6e714e4c0f64d97f864f
rauno <raunoraa13@gmail.com>
2025-02-12 17:02:18 +0200

    Additional fixes.

--- practice_4/nodes/control/pure_pursuit_follower.py
+++ practice_4/nodes/control/pure_pursuit_follower.py
@@ -2,6 +2,7 @@
 
 import rospy
 import numpy as np
+import threading
 
 from autoware_mini.msg import Path, VehicleCmd
 from geometry_msgs.msg import PoseStamped
@@ -22,8 +23,11 @@ class PurePursuitFollower:
         self.wheel_base = rospy.get_param("/vehicle/wheel_base")
 
         self.path_var = None
-
         self.distance_to_velocity_interpolator = None
+        
+        # Necessary for ensuring that the variables are assigned at the same time
+        # because ROS is multithreaded.
+        self.lock = threading.Lock()
 
         # Publishers
         self.vehicle_cmd_pub = rospy.Publisher("vehicle_cmd", VehicleCmd, queue_size=10)
@@ -39,18 +43,21 @@ class PurePursuitFollower:
 
     def path_callback(self, msg):
 
+        # Loop only once over the msg.waypoints
+        waypoints_xy = []
+        velocities = []
+        for w in msg.waypoints:
+            waypoints_xy.append((w.position.x, w.position.y))
+            velocities.append(w.speed)
+        
         # convert waypoints to shapely linestring
-        path_linestring = LineString(
-            [(w.position.x, w.position.y) for w in msg.waypoints]
-        )
+        path_linestring = LineString(waypoints_xy)
         # prepare path - creates spatial tree, making the spatial queries more efficient
         prepare(path_linestring)
 
-        self.path_var = path_linestring
-
         # Create a distance to velocity interpolator for the path
         # collect waypoint x and y coordinates
-        waypoints_xy = np.array([(w.position.x, w.position.y) for w in msg.waypoints])
+        waypoints_xy = np.array(waypoints_xy)
         # Calculate distances between points
         distances = np.cumsum(
             np.sqrt(np.sum(np.diff(waypoints_xy, axis=0) ** 2, axis=1))
@@ -58,47 +65,58 @@ class PurePursuitFollower:
         # add 0 distance in the beginning
         distances = np.insert(distances, 0, 0)
         # Extract velocity values at waypoints
-        velocities = np.array([w.speed for w in msg.waypoints])
-        self.distance_to_velocity_interpolator = interp1d(
-            distances, velocities, kind="linear"
+        velocities = np.array(velocities)
+        
+        distance_to_velocity_interpolator = interp1d(
+                distances, velocities, kind="linear", bounds_error=False, fill_value=0.0
         )
+        
+        with self.lock:        
+            self.path_var = path_linestring
+            self.distance_to_velocity_interpolator = distance_to_velocity_interpolator
 
     def current_pose_callback(self, msg):
+        
+        # Check if necessary variables are not None
+        if self.path_var is None or self.distance_to_velocity_interpolator is None:
+            return
+        
+        with self.lock:
+            path_linestring = self.path_var
+            distance_to_velocity_interpolator = self.distance_to_velocity_interpolator
+        
+        current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+        
+        d_ego_from_path_start = path_linestring.project(current_pose)
+        _, _, heading = euler_from_quaternion(
+            [
+                msg.pose.orientation.x,
+                msg.pose.orientation.y,
+                msg.pose.orientation.z,
+                msg.pose.orientation.w,
+            ]
+        )
+
+        lookahead_point = self.path_var.interpolate(
+            d_ego_from_path_start + self.lookahead_distance
+        )
+        lookahead_heading = np.arctan2(
+            lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x
+        )
+
+        l_d = distance(current_pose, lookahead_point)
+        alpha = lookahead_heading - heading
+        steering_angle = np.arctan(
+            (2 * self.wheel_base * np.sin(alpha)) / l_d
+        )
+        
+        linear_velocity = distance_to_velocity_interpolator(d_ego_from_path_start)
+        
         vehicle_cmd = VehicleCmd()
         vehicle_cmd.header.stamp = msg.header.stamp
         vehicle_cmd.header.frame_id = "base_link"
-
-        current_pose = Point([msg.pose.position.x, msg.pose.position.y])
-        if self.path_var:
-            d_ego_from_path_start = self.path_var.project(current_pose)
-            _, _, heading = euler_from_quaternion(
-                [
-                    msg.pose.orientation.x,
-                    msg.pose.orientation.y,
-                    msg.pose.orientation.z,
-                    msg.pose.orientation.w,
-                ]
-            )
-
-            lookahead_point = self.path_var.interpolate(
-                d_ego_from_path_start + self.lookahead_distance
-            )
-            lookahead_heading = np.arctan2(
-                lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x
-            )
-
-            l_d = distance(current_pose, lookahead_point)
-            alpha = lookahead_heading - heading
-
-            vehicle_cmd.ctrl_cmd.steering_angle = np.arctan(
-                (2 * self.wheel_base * np.sin(alpha)) / l_d
-            )
-
-            if self.distance_to_velocity_interpolator:
-                vehicle_cmd.ctrl_cmd.linear_velocity = (
-                    self.distance_to_velocity_interpolator(d_ego_from_path_start)
-                )
-
+        vehicle_cmd.ctrl_cmd.steering_angle = steering_angle
+        vehicle_cmd.ctrl_cmd.linear_velocity = linear_velocity
         self.vehicle_cmd_pub.publish(vehicle_cmd)
 
     def run(self):
@@ -112,6 +130,9 @@ if __name__ == "__main__":
 
 """
 Vastus küsimusele:
+Run the code with different lookahead distance by changing lookahead_distance: 20.0 in control.yaml. 
+Is the behaviour different? What is different and why?
+
 Suurema lookahead distance puhul hakkab masin kurve lõikama, 
 sest suurema lookahead distance puhul on järgmine waypoint tee peal alles peale kurvi.
 Sõiduk üritab võimalikult otse liikuda järgmise waypointini, mistõttu ei arvesta sõiduk vahepealse reaalse teega.
